0359ec68b794e20a4e46fa5c78070820b8cfa8ab
Kennedy Kinoti <kennedy.kinoti@pwc.com>
2021-12-13 16:24:26 +1030

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# python-CRUD
+A simple python API based of the fast API python library to familiarize self with the fundamentals
+
+## Setup
+- Docker
+- Coder
+- Python packages
+    - FastAPI
+    - Optional
+
+## Coder 
+
+s
---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Dunia"}
+
+@app.post("/todo")
+def create_item():
+    return {"Create todo item"}
+
+@app.get("/todo/{id}")
+def read_todo(id: int, q: Optional[str] = None):
+    return {"item_id": id, "q": q}
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return {"Update the todo item with id {id}"}
+
+@app.delete("/")
+def delete_todo(id: int):
+    return {"Delete todo item with id {id}"}
